359fc12a7a0f9ecbd403c7915e239d3a9515260c
klothes <115721449+kloths@users.noreply.github.com>
2025-09-05 16:27:42 +0100

    chore: add plan interface to types

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -5,3 +5,12 @@ export interface Feature {
   description: string;
   icon: LucideIcon;
 }
+
+export interface Plan {
+  name: string;
+  description?: string;
+  price: number | null;
+  features: string[];
+  includesPlan?: string;
+  billing: "one-time" | "monthly" | "annual";
+}
